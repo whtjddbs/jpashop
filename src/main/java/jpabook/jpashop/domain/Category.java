e25e43e6ba93d6f012229a7af3d5d316eb444b01
whtjddbs <jsy_1201@naver.com>
2024-02-21 23:24:45 +0900

    요구사항에 따른 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -1,4 +1,28 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+//    private List<Item> items = new ArrayList<>();
+
+//    private Category parent;
 }
